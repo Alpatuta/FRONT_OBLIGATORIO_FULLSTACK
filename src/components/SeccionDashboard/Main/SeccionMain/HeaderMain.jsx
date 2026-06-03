5ef09b7510fb586270bf392d39b3339015509a33
foteiza <fede.oteiza@gmail.com>
2026-06-03 13:54:47 -0300

    feat: - Actualizando el display del usuario para que use los datos reales
    - Agregando el cerrar sesion al botón correspondiente

--- src/components/SeccionDashboard/Main/SeccionMain/HeaderMain.jsx
+++ src/components/SeccionDashboard/Main/SeccionMain/HeaderMain.jsx
@@ -1,4 +1,14 @@
+import { useSelector } from "react-redux";
+
 const HeaderMain = ({ eyebrow, title }) => {
+  const user = useSelector((state) => state.auth.user);
+  const primerLetraNombre = user?.nombre
+    ? user.nombre.charAt(0).toUpperCase()
+    : "U";
+  const primerLetraApellido = user?.nombre
+    ? user.nombre.split(" ").slice(-1)[0].charAt(0).toUpperCase()
+    : "N";
+
   return (
     <header className="topbar">
       <div className="topbar-left">
@@ -6,10 +16,13 @@ const HeaderMain = ({ eyebrow, title }) => {
         <h1 id="dashboard-title">{title}</h1>
       </div>
       <div className="profile-pill">
-        <div className="p-avatar">RP</div>
+        <div className="p-avatar">
+          {primerLetraNombre}
+          {primerLetraApellido}
+        </div>
         <div>
-          <strong>Rodrigo</strong>
-          <span>Plan plus</span>
+          <strong>{user?.nombre}</strong>
+          <span>Plan {user?.plan}</span>
         </div>
       </div>
     </header>
--- src/components/SeccionDashboard/Sidebar/Sidebar.jsx
+++ src/components/SeccionDashboard/Sidebar/Sidebar.jsx
@@ -1,21 +1,40 @@
 import { Link } from "react-router";
 import HeaderSidebar from "./HeaderSidebar";
 import NavbarSidebar from "./NavbarSidebar";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../../features/auth/auth.slice";
 
 const Sidebar = () => {
+  const token = useSelector((state) => state.auth.token);
+  const user = useSelector((state) => state.auth.user);
+  const primerLetraNombre = user?.nombre
+    ? user.nombre.charAt(0).toUpperCase()
+    : "U";
+  const primerLetraApellido = user?.nombre
+    ? user.nombre.split(" ").slice(-1)[0].charAt(0).toUpperCase()
+    : "N";
+  const dispatch = useDispatch();
+  const cerrarSesion = () => {
+    localStorage.clear();
+    dispatch(logout((token = null), (user = null), (isAuthenticated = false)));
+  };
+
   return (
     <aside className="sidebar">
       <HeaderSidebar />
       <NavbarSidebar />
       <div className="sidebar-footer">
         <div className="sidebar-user">
-          <div className="sidebar-user-avatar">RP</div>
+          <div className="sidebar-user-avatar">
+            {primerLetraNombre}
+            {primerLetraApellido}
+          </div>
           <div>
-            <div className="sidebar-user-name">Rodrigo Pintos</div>
-            <div className="sidebar-user-plan">Plan plus</div>
+            <div className="sidebar-user-name">{user?.nombre}</div>
+            <div className="sidebar-user-plan">Plan {user?.plan}</div>
           </div>
         </div>
-        <Link to="/" className="btn-sidebar-logout">
+        <Link to="/" className="btn-sidebar-logout" onClick={cerrarSesion}>
           <svg
             viewBox="0 0 24 24"
             fill="none"
