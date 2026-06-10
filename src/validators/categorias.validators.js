3f9062a8bb93512ee29899694beef66993d8cf71
Magodnes <ropin1697@gmail.com>
2026-06-10 08:22:52 -0300

    FIX: mejorar mensajes de validación en los esquemas de categoría

--- src/validators/categorias.validators.js
+++ src/validators/categorias.validators.js
@@ -3,21 +3,26 @@ import joi from "joi";
 export const categoriaSchema = joi.object({
     nombre: joi.string().min(3).required().messages({
         "string.base": "El nombre debe ser una cadena de texto",
-        "string.empty": "El nombre no puede estar vacío"
+        "string.empty": "El nombre no puede estar vacío",
+        "string.min": "El nombre debe tener al menos 3 caracteres"
+
     }),
     descripcion: joi.string().min(5).required().messages({
         "string.base": "La descripción debe ser una cadena de texto",
-        "string.empty": "La descripción no puede estar vacía"
+        "string.empty": "La descripción no puede estar vacía",
+        "string.min": "La descripción debe tener al menos 5 caracteres"
     })
 });
 
 export const actualizarCategoriaSchema = joi.object({
     nombre: joi.string().min(3).messages({
         "string.base": "El nombre debe ser una cadena de texto",
-        "string.empty": "El nombre no puede estar vacío"
+        "string.empty": "El nombre no puede estar vacío",
+        "string.min": "El nombre debe tener al menos 3 caracteres"
     }),
     descripcion: joi.string().min(5).messages({
         "string.base": "La descripción debe ser una cadena de texto",
-        "string.empty": "La descripción no puede estar vacía"
+        "string.empty": "La descripción no puede estar vacía",
+        "string.min": "La descripción debe tener al menos 5 caracteres"
     })
 });
